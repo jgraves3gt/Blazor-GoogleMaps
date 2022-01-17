87e84a1d3b1baad5ec0accef64f26178e0b574ac
Joe Graves <jgraves@3rdgenmachine.com>
2022-01-17 14:03:39 -0700

    Got the maps working

---
+++ Blazor-GoogleMaps/Models/MapPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_GoogleMaps.Models
+{
+    public class MapPoint
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+}
---
+++ Blazor-GoogleMaps/wwwroot/js/map.js
@@ -0,0 +1,36 @@
+﻿let maps = [];
+
+function initMap(mapId, lat, lon) {
+    try {
+        var map = new google.maps.Map(document.getElementById(mapId), {
+            center: { lat: lat, lng: lon },
+            zoom: 8,
+            mapId: 'dd7254a7eea796d1'
+        });
+
+        maps[mapId] = map;
+        
+    }
+    catch (e) {
+        console.log("Map Initialization Error: " + e);
+    }
+}
+
+function setMapClickListener(MapId, DNMapRef) {
+    var map = maps[MapId];
+
+    map.addListener("click", (mapsMouseEvent) => {
+        //console.log("From JS - Lat:" + mapsMouseEvent.latLng.lat() + " | Lon:" + mapsMouseEvent.latLng.lng());
+        DNMapRef.invokeMethodAsync('MapClicked', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+    });
+
+    map.addListener("dblclick", (mapsMouseEvent) => {
+        //console.log("From JS - Lat:" + mapsMouseEvent.latLng.lat() + " | Lon:" + mapsMouseEvent.latLng.lng());
+        DNMapRef.invokeMethodAsync('MapDoubleClicked', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+    });
+
+    map.addListener("rightclick", (mapsMouseEvent) => {
+        //console.log("From JS, Right Clicked - Lat:" + mapsMouseEvent.latLng.lat() + " | Lon:" + mapsMouseEvent.latLng.lng());
+        DNMapRef.invokeMethodAsync('MapRightClicked', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+    });
+}
