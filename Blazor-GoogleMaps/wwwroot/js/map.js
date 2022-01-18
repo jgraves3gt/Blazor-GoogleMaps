65076717946714404555b0f9488d086dcbd1c348
joeygraves222 <joeygraves222@gmail.com>
2022-01-18 08:04:19 -0700

    Finished Map Events

--- Blazor-GoogleMaps/wwwroot/js/map.js
+++ Blazor-GoogleMaps/wwwroot/js/map.js
@@ -16,21 +16,86 @@ function initMap(mapId, lat, lon) {
     }
 }
 
+function addMapMarker(mapId, marker) {
+
+}
+
 function setMapClickListener(MapId, DNMapRef) {
     var map = maps[MapId];
 
     map.addListener("click", (mapsMouseEvent) => {
-        //console.log("From JS - Lat:" + mapsMouseEvent.latLng.lat() + " | Lon:" + mapsMouseEvent.latLng.lng());
         DNMapRef.invokeMethodAsync('MapClicked', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
     });
 
     map.addListener("dblclick", (mapsMouseEvent) => {
-        //console.log("From JS - Lat:" + mapsMouseEvent.latLng.lat() + " | Lon:" + mapsMouseEvent.latLng.lng());
         DNMapRef.invokeMethodAsync('MapDoubleClicked', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
     });
 
     map.addListener("rightclick", (mapsMouseEvent) => {
-        //console.log("From JS, Right Clicked - Lat:" + mapsMouseEvent.latLng.lat() + " | Lon:" + mapsMouseEvent.latLng.lng());
         DNMapRef.invokeMethodAsync('MapRightClicked', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+    });
+
+    map.addListener("mousemove", (mapsMouseEvent) => {
+        DNMapRef.invokeMethodAsync('MapMouseMove', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+    });
+
+    map.addListener("mouseout", (mapsMouseEvent) => {
+        DNMapRef.invokeMethodAsync('MapMouseOut', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+    });
+
+    map.addListener("mouseover", (mapsMouseEvent) => {
+        DNMapRef.invokeMethodAsync('MapMouseOver', mapsMouseEvent.latLng.lat(), mapsMouseEvent.latLng.lng());
+    });
+
+    map.addListener("center_changed", () => {
+        DNMapRef.invokeMethodAsync('MapCenterChanged');
+    });
+
+    map.addListener("drag", () => {
+        DNMapRef.invokeMethodAsync('MapDrag');
+    });
+
+    map.addListener("dragend", () => {
+        DNMapRef.invokeMethodAsync('MapDragEnd');
+    });
+
+    map.addListener("dragstart", () => {
+        DNMapRef.invokeMethodAsync('MapDragStart');
+    });
+
+    map.addListener("heading_changed", () => {
+        DNMapRef.invokeMethodAsync('MapHeadingChanged');
+    });
+
+    map.addListener("idle", () => {
+        DNMapRef.invokeMethodAsync('MapIdle');
+    });
+
+    map.addListener("maptypeid_changed", () => {
+        DNMapRef.invokeMethodAsync('MapMapTypeIdChanged');
+    });
+
+    map.addListener("projection_changed", () => {
+        DNMapRef.invokeMethodAsync('MapProjectionChanged');
+    });
+
+    map.addListener("resize", () => {
+        DNMapRef.invokeMethodAsync('MapResize');
+    });
+
+    map.addListener("bounds_changed", () => {
+        DNMapRef.invokeMethodAsync('MapBoundsChanged');
+    });
+
+    map.addListener("tilesloaded", () => {
+        DNMapRef.invokeMethodAsync('MapTilesLoaded');
+    });
+
+    map.addListener("tilt_changed", () => {
+        DNMapRef.invokeMethodAsync('MapTiltChanged');
+    });
+
+    map.addListener("zoom_changed", () => {
+        DNMapRef.invokeMethodAsync('MapZoomChanged');
     });
 }
